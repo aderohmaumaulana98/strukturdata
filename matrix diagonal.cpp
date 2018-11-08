7fd4dc1ebf01e806177aae41b5aff33f72d43ba8
ade rohmat maulana <43772130+aderohmaumaulana98@users.noreply.github.com>
2018-11-08 17:24:39 +0700

    Add files via upload
    Ade rohmat maulana
    5170411330

---
+++ matrix diagonal.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	int i,j,k,m[50][50];
+	
+	cout<<"masukan jumlah ordo : ";
+	cin>>i;
+	cout<<"\n\n";
+	for (j=0; j<i; j++){
+		for (k=0; k<i; k++){
+			cout<<"["<<j<<"]["<<k<<"] : ";
+			cin>>m[j][k];
+			if(j+k==i-1){
+				m[j][k]=0;
+			
+		}
+		}
+	}
+	cout<<endl;
+	cout<<endl;
+	for (j=0;j<i;j++){
+		for (k=0;k<i;k++){
+			cout<<m[j][k]<<"  ";
+	}
+	cout<<"\n";
+}
+getch();
+}
